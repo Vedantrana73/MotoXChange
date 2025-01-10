1353b948aa2f30806c84d65c454e620dcc747906
Akshat Gohil <akshatgohil4243@gmail.com>
2025-01-10 23:02:27 +0530

    Added OTP Verification Functionality

--- Client/src/Components/RegisterAuthentication.tsx
+++ Client/src/Components/RegisterAuthentication.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import { useUser } from '../context/UserContext';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterAuthentication: React.FC = () => {
   const {email} = useUser();
   const [otp, setOtp] = useState<string[]>(['', '', '', '']);
+  const [successMessage,setSuccessMessage] = useState<String>('');
+  const [errorMessage,setErrorMessage] = useState<String>('');
 
+  const navigate = useNavigate();
   const handleOtpChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
     const value = e.target.value;
     if (/^[0-9]$/.test(value) || value === '') {
@@ -23,6 +28,31 @@ const RegisterAuthentication: React.FC = () => {
     if(index> 0 && event.key === "Backspace" && otp[index]==='')
     {
       document.getElementById(`otp-box-${index-1}`)?.focus();
+    }
+  }
+
+  const verifyOtp = async() =>{
+    try
+    {
+      const response = await axios.post('http://localhost:5000/user/verify-otp',{
+        email,
+        otp: otp.join('')
+      });
+      if(response.data)
+      {
+        setSuccessMessage(response.data.message);
+        setTimeout(() => {
+          setSuccessMessage('');
+          navigate('/login');
+        }, 3000);
+      }
+    }
+    catch(error: any)
+    {
+      setErrorMessage(error.response.data.message);
+      setTimeout(() => {
+        setErrorMessage('')
+      }, 4000);
     }
   }
   return (
@@ -48,10 +78,16 @@ const RegisterAuthentication: React.FC = () => {
         ))}
       </div>
       <div>
-        <button className="block w-72 py-4 font-semibold text-xl bg-black text-white hover:text-black hover:bg-gray-200 hover:shadow-xl transition-colors duration-300">
+        <button className="block w-72 py-4 font-semibold text-xl bg-black text-white hover:text-black hover:bg-gray-200 hover:shadow-xl transition-colors duration-300" onClick={verifyOtp}>
           Verify
         </button>
       </div>
+      {
+        successMessage && <div className='text-lg text-green-500 font-semibold'>{successMessage}</div>
+      }
+      {
+        errorMessage && <div className='text-lg text-red-500 font-semibold'>{errorMessage}</div>
+      }
     </div>
   );
 };
